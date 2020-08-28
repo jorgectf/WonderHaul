a4338b9eedcbf5940c2da0af57243c0a61ceaad3
James Reynolds <jamesreynoldsjobs@gmail.com>
2020-08-28 15:09:59 +0100

    Missing changes

--- WinterCoreUtils/src/main/java/io/github/winterbear/WinterCoreUtils/ChatUtils.java
+++ WinterCoreUtils/src/main/java/io/github/winterbear/WinterCoreUtils/ChatUtils.java
@@ -6,18 +6,47 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by WinterBear on 20/05/2019.
  */
 public class ChatUtils {
 
+    public static final Map<String, String> COLOR_NAMES = colorNames();
+
+    private static Map<String, String> colorNames(){
+
+        Map<String, String> colors = new HashMap<>();
+
+        colors.put("Black", "&0");
+        colors.put("Blue", "&1");
+        colors.put("Green", "&2");
+        colors.put("Cyan", "&3");
+        colors.put("Red", "&4");
+        colors.put("Purple", "&5");
+        colors.put("Gold", "&6");
+        colors.put("Gray", "&7");
+        colors.put("Dark Gray", "&8");
+        colors.put("Light Blue", "&9");
+        colors.put("Lime", "&a");
+        colors.put("Aqua", "&b");
+        colors.put("Light Red", "&c");
+        colors.put("Pink", "&d");
+        colors.put("Yellow", "&e");
+        colors.put("White", "&f");
+
+        return colors;
+
+    }
+
     private static final Character MODIFIER_UNCONVERTED = '&';
 
     private static final Character MODIFIER_CONVERTED = '§';
 
-    private static final String REGEX_COLOR_CODES = "[&§][abcdef0123456789lmnok]";
+    private static final String REGEX_COLOR_CODES = "[&§][a-z,A-Z,0-9]";
 
     public static final List<String> COLOR_CODES = Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f");
 
@@ -34,7 +63,7 @@ public class ChatUtils {
     }
 
     public static String uncolored(String text){
-        return text.replaceAll(REGEX_COLOR_CODES, EMPTY_STRING);
+        return ChatColor.stripColor(text.replaceAll(REGEX_COLOR_CODES, EMPTY_STRING));
     }
 
     public static String format(String message){
